72e823d0ad1b91af7278a55c9e0657e8e5bc4fc9
colekrstinich22 <36930194+colekrstinich22@users.noreply.github.com>
2018-03-08 12:01:09 -0600

    Add files via upload

--- Earth.py
+++ Earth.py
@@ -1,6 +1,20 @@
-class Earth(object):
+from Planet import *
 
-    def buildPlanet(self,radius,color,center:Point):
-        self.radius = radius
-        self.color = color
-        self.center = center
+class Earth(Planet):
+
+    def __init__(self,radius,color,centerX,centerY,continent,window):
+        super(Earth, self).__init__(radius,color,centerX,centerY,window)
+        self.continent = continent
+        self.buildEarth(window)
+
+    def buildEarth(self,win):
+        pass
+
+
+win = GraphWin("Test", 1000, 650)
+earth = Earth(100, "blue", 400, 200, "uhhhhhhh", win)
+
+win.getMouse()
+win.close()
+
+
